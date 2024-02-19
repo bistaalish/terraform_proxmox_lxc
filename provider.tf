56b4739b76a6d4210d765b8d19591d0400db2d20
Alish Bista <bistace321@gmail.com>
2024-02-19 17:51:23 +0545

    Feat: Written Provider

--- provider.tf
+++ provider.tf
@@ -11,4 +11,5 @@ provider "proxmox" {
   pm_api_url = var.proxmox_api_url
   pm_api_token_secret = var.proxmox_api_token_secret
   pm_api_token_id = var.proxmox_api_token_id
+  pm_tls_insecure = true
 }
